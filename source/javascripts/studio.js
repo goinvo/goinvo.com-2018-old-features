832be2b20db590e43cfb5ff6af62a297ea96c482
Adam Pere <adam@goinvo.com>
2014-05-09 12:02:21 -0400

    studio.js order of function calls

--- source/javascripts/studio.js
+++ source/javascripts/studio.js
@@ -1,17 +1,6 @@
 var userListData = [];
 var social2 = "";
 
-$(document).ready(function() {
-    console.log("document  ready");
-     // jQuery AJAX call for JSON
-    $.getJSON( 'http://goinvo-api.herokuapp.com/users/userlist', function( data ) {
-        userListData = data;
-    });
-    // Populate the user table on initial page load
-    populateSocialMedia(25, 0);
-});
-
-
 
 
 
@@ -23,9 +12,7 @@ function populateSocialMedia() {
         $.each(data, function(){
             var name = "";
             var temp = this;
-            var link = 'http://goinvo-api.heroku.com/social/username/' + this.username + "/" + this.type;
-          
-            console.log(getName(this));
+
             social2 += generateTableData(name, temp);
             $('#social-media').html(social2);
             $( ".social-card" ).click(function() {
@@ -43,7 +30,6 @@ function populateSocialMedia(listLength, offset) {
         $.each(data, function(){
             var name = "";
             var temp = this;
-            var link = 'http://goinvo-api.herokuapp.com/social/username/' + this.username + "/" + this.type;
             
             $.when(getName(this)).then(function(name2) {
                 console.log(name2);
@@ -54,32 +40,45 @@ function populateSocialMedia(listLength, offset) {
         $( ".social-card" ).click(function() {
             window.open(this.dataset.link,'_blank');
         });
+    }).done(function() {
+        $('.social-card').click(function(){
+              window.location = $(this).data('link');
+            });
+
+            $('#the-studio .content').masonry({
+              columnWidth: '.social-card.photo',
+              itemSelector: '.social-card',
+              gutter: 0,
+              isFitWidth: true, 
+              animate: true
+            });
     });
 }
 
 function getName(data) {
     var username = data.username;
     var type = data.type;
-    var name = "";
+    var name = data;
+    console.log(userListData);
     $.each(userListData, function() {
         if(type === "twitter") {
            if(this.twitter == username){
-                name = this.name;   
+                name = this;   
            }
         }
         else if(type == "flickr") {
             if(this.flickr == username){
-                name = this.name;   
+                name = this;   
            }
         }
         else if (type == "github") {
             if(this.github == username){
-                name = this.name;   
+                name = this;   
            }
         }
         else if (type == "soundcloud") {
             if(this.soundcloud == username){
-                name = this.name;   
+                name = this;   
            }
         }
     });
@@ -88,15 +87,15 @@ function getName(data) {
 
 
 
-function generateTableData(name, data) {
+function generateTableData(user, data) {
       // Empty content string
     var social = "";
     var temp = "";
     var start = 0;
     var stop = 0;
-    
+    console.log(user);
     if(data.type == "twitter") {
-        social += "<div class = 'social-card tweet' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar'>" + name+  "<br> " + data.date + "</div></div>";
+        social += "<div class = 'social-card tweet' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar'>" + user.avatar+  "<br> " + data.date + "</div></div>";
     }
     else if(data.type == "flickr") {
         temp = data.content_embed;
@@ -104,14 +103,23 @@ function generateTableData(name, data) {
         temp = temp.substring(start);
         stop = temp.indexOf(">")+1;
         temp = temp.substring(0,stop);
-        social += "<div class = 'social-card photo' data-link = '" + data.url + "'>" + temp + "<p class = 'caption'>" + data.content + "</p><div class = 'avatar'>"+ name +"<br>" + data.date + "</div></div>";
+        social += "<div class = 'social-card photo' data-link = '" + data.url + "'>" + temp + "<p class = 'caption'>" + data.content + "</p><div class = 'avatar'>"+ user.avatar +"<br>" + data.date + "</div></div>";
     }
     else if(data.type == "github") {
-        social += "<div class = 'social-card github' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar'>" + name+"<br>" + data.date +"</div></div>";
+        social += "<div class = 'social-card github' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar'>" + user.avatar+"<br>" + data.date +"</div></div>";
     }
     else if(this.type === "soundcloud") {
-        social += "<div class = 'social-card soundcloud' data-link = '" + data.url + "'>" + data.content_embed + "<p class = 'caption'>" + data.content +" " + data.date + "<div class = 'avatar'>"+ name + "<br>" + data.date +"</div></div>";
+        social += "<div class = 'social-card soundcloud' data-link = '" + data.url + "'>" + data.content_embed + "<p class = 'caption'>" + data.content +" " + data.date + "<div class = 'avatar'>"+ user.avatar + "<br>" + data.date +"</div></div>";
     }
     return social;
    
 }
+
+$(document).ready(function() {
+    console.log("document  ready");
+     // jQuery AJAX call for JSON
+    
+    $.getJSON( 'http://goinvo-api.herokuapp.com/users/userlist', function( data ) { userListData = data;}).done(function() {
+       populateSocialMedia(25, 0)
+    });
+});
