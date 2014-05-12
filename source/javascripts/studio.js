666d044bc97d3fc9f995638a64ecee3ab2609ffc
Adam Pere <adam@goinvo.com>
2014-05-12 17:03:42 -0400

    Studio page
    Updated API and this js accordingly. There should be at east 50+
    events. They are all ordered by date created. Scroll to the bottom to
    load the next 25.

--- source/javascripts/studio.js
+++ source/javascripts/studio.js
@@ -1,33 +1,31 @@
-var userListData = [];
-var social2 = "";
 var offsetNumber = 0;
-
+var numEvents = 0;
 
 // Combines all events' html together & pushes it to the web page
 function populateSocialMedia(listLength, offset) {
-    $.getJSON( 'http://goinvo-api.herokuapp.com/social/events/' + listLength + '/' + offset, function( data ) {
-        console.log(data);
+    $.getJSON( 'http://goinvo-api.herokuapp.com/v1/social/events/' + listLength + '/' + offset, function( data ) {
+        
         // Aggregate html for all events to be displayed
         $.each(data, function(){
+            numEvents++;
             var temp = this;
-            $.when(getUser(this)).then(function(user2) {social2 += generateEventHTML(user2, temp);}); //Make sure that we have the user before generating html
+            $.when(getUser(this)).then(function(user2) {
+                $('#the-studio div.content').append(generateEventHTML(temp));
+            }); //Make sure that we have the user before generating html
         });
-        $('#the-studio div.content').append(social2);
-    
         $( ".social-card.photo, .social-card.tweet, .social-card.github " ).click(function() { window.open(this.dataset.link,'_blank'); }); //Makes all social cards links (open in new tab)
         
     }).done(function() { //Masonry is not initialized until after the events' html is completely loaded ---- Controls the page's grid
-            $('#the-studio .content').masonry({
-              columnWidth: '.social-card.photo', //The width of the first card sets the column width (note: all .social-card are the same width)
+            
+        $('#the-studio .content').masonry({
+              columnWidth: '.social-card', //The width of the first card sets the column width (note: all .social-card are the same width)
               itemSelector: '.social-card',
               gutter: 0, //Use margins instead. I found that this makes the grid much more consistent
               isFitWidth: true, 
               animate: true
             });
-        
-         $('#the-studio .content').masonry( 'reloadItems' );
+        $('#the-studio .content').masonry( 'reloadItems' );
         $('#the-studio .content').masonry( 'layout' );
-        social2 = ""; //comment out to duplicate current events w/ every scroll
     });
 }
 
@@ -36,41 +34,20 @@ function getUser(data) {
     var username = data.username;
     var type = data.type;
     var name = data;
-    $.each(userListData, function() {
-        if(type === "twitter") {
-           if(this.twitter == username){
-                name = this;   
-           }
-        }
-        else if(type == "flickr") {
-            if(this.flickr == username){
-                name = this;   
-           }
-        }
-        else if (type == "github") {
-            if(this.github == username){
-                name = this;   
-           }
-        }
-        else if (type == "soundcloud") {
-            if(this.soundcloud == username){
-                name = this;   
-           }
-        }
-    });
-    return name;
+  
+    return data.user.name;
 }
 
 
 //Returns the html for an event's social-card
-function generateEventHTML(user, data) {
+function generateEventHTML( data) {
     var social = "";
     var temp = "";
     var start = 0;
     var stop = 0;
     
     if(data.type == "twitter") {
-        social += "<div class = 'social-card tweet' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar' style = 'background-image:url(" + user.avatar + ")'></div></div>";
+        social += "<div class = 'social-card tweet' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar' style = 'background-image:url(" + data.user.avatar + ")'></div></div>";
     }
     else if(data.type == "flickr") {
         temp = data.content_embed;
@@ -78,21 +55,20 @@ function generateEventHTML(user, data) {
         temp = temp.substring(start);
         stop = temp.indexOf("width") -2;
         temp = temp.substring(0,stop);
-        
-        social += "<div class = 'social-card photo'  style = 'background-image:url(" + temp + ")' data-link = '" + data.url + "' ><p class = 'caption'>" + data.content + "</p><div class = 'avatar' style = 'background-image:url(" + user.avatar + ")'></div></div>";
+        social += "<div class = 'social-card photo'  style = 'background-image:url(" + temp + ")' data-link = '" + data.url + "' ><p class = 'caption'>" + data.content + "</p><div class = 'avatar' style = 'background-image:url(" + data.user.avatar + ")'></div></div>";
     }
     else if(data.type == "github") {
-        social += "<div class = 'social-card github' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar' style = 'background-image:url(" + user.avatar + ")'></div></div>";
+        social += "<div class = 'social-card github' data-link = '" + data.url + "'> <p class = 'contents'>" + data.content + "</p><div class = 'avatar' style = 'background-image:url(" + data.user.avatar + ")'></div></div>";
     }
     else if(data.type == "soundcloud") {
-        social += "<div class = 'social-card soundcloud' >" + data.content_embed + "<p class = 'contents'>" + data.content + "<div class = 'avatar' style = 'background-image:url(" + user.avatar + ")'></div></div>";
+        social += "<div class = 'social-card soundcloud' >" + data.content_embed + "<p class = 'contents'>" + data.content + "<div class = 'avatar' style = 'background-image:url(" + data.user.avatar + ")'></div></div>";
     }
     return social;
 }
 
 $(document).ready(function() {
     //Get the list of users & save it to a global variable, then populate the social media
-    $.getJSON( 'http://goinvo-api.herokuapp.com/users/userlist', function( data ) { userListData = data;}).done(function() {
+    $.getJSON( 'http://goinvo-api.herokuapp.com/v1/users/userlist', function( data ) { }).done(function() {
        populateSocialMedia(25, offsetNumber)
     });
 });
@@ -102,6 +78,9 @@ $(function(){
        if($(document).height()==$(window).scrollTop()+$(window).height()){
            offsetNumber += 25;
            populateSocialMedia(25, offsetNumber);
+           if(numEvents%25 != 0) {
+                $('.bottom').html("You've reached the end!");   
+           }
        }
    });
 });
