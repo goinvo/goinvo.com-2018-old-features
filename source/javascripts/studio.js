bd3a61040f8b970aaa1267329fdb64fd5490602a
Adam Pere <adam@goinvo.com>
2014-05-13 18:16:43 -0400

    removed redundant code & added comments

--- source/javascripts/studio.js
+++ source/javascripts/studio.js
@@ -3,44 +3,20 @@ var numEvents = 0;
 
 // Combines all events' html together & pushes it to the web page
 function populateSocialMedia(listLength, offset) {
+    //retrieve events & push their html to the studio page
     $.getJSON( 'http://goinvo-api.herokuapp.com/v1/social/events/' + listLength + '/' + offset, function( data ) {
-        
-        // Aggregate html for all events to be displayed
         $.each(data, function(){
             numEvents++;
-            var temp = this;
-            $.when(getUser(this)).then(function(user2) {
-                $('#the-studio div.content').append(generateEventHTML(temp));
-            }); //Make sure that we have the user before generating html
+            $('#the-studio div.content').append(generateEventHTML(this));
         });
         $( ".social-card.photo " ).click(function() { window.open(this.dataset.link,'_blank'); }); //Makes all social cards links (open in new tab)
         
     }).done(function() { //Masonry is not initialized until after the events' html is completely loaded ---- Controls the page's grid
-        
-            $('#the-studio .content').masonry({
-              columnWidth: '.social-card', //The width of the first card sets the column width (note: all .social-card are the same width)
-              itemSelector: '.social-card',
-              gutter: 0, //Use margins instead. I found that this makes the grid much more consistent
-              isFitWidth: true, 
-              animate: true
-            });
-            
             $('#the-studio .content').masonry( 'reloadItems' );$('#the-studio .content').masonry( 'layout' );
+            //I don't like doing this but I'm waiting 500ms after and then reloading the grid. This is to (hopefully ensure that the newest batch of images has been loaded [and therefore masonry can get it's width and height].
             setTimeout( function() { $('#the-studio .content').masonry( 'reloadItems' );$('#the-studio .content').masonry( 'layout' );}, 500);
-            
-    
     });
 }
-
-//returns the user who the event belongs to
-function getUser(data) {
-    var username = data.username;
-    var type = data.type;
-    var name = data;
-  
-    return data.user.name;
-}
-
 
 //Returns the html for an event's social-card
 function generateEventHTML( data) {
@@ -62,7 +38,7 @@ function generateEventHTML( data) {
         start = temp.indexOf("<img src=") +10;
         temp = temp.substring(start);
         stop = temp.indexOf("width") -2;
-        temp = temp.substring(0,stop);
+        temp = temp.substring(0,stop); //remove flickr's generated html so I can do my own
         social += "<div class = 'social-card photo' data-link = '" + data.url + "' ><img class = 'flickr-photo' src = '" + temp + "' width = '100%' height = 'auto'><p class = 'caption'>" + data.content + "</p><div class = 'social-links'><img src = '../images/flickr_foot_black.png' width = '25px' height = 'auto'></div> <a href = '" + data.url + "' target = '_blank' class = 'social-handle'>&#64;" + data.username  + "<br>" +  new Date(data.date).toLocaleDateString("en-US") + "</a><a href ='" + data.url + "'><div class = 'avatar' style = 'background-image:url(" + data.user.avatar + ")'></div></a></div>";
     }
     else if(data.type == "github") {
@@ -75,17 +51,24 @@ function generateEventHTML( data) {
 }
 
 $(document).ready(function() {
-    //Get the list of users & save it to a global variable, then populate the social media
-    $.getJSON( 'http://goinvo-api.herokuapp.com/v1/users/userlist', function( data ) { }).done(function() {
-       populateSocialMedia(25, offsetNumber)
+    //initialize the grid
+    $('#the-studio .content').masonry({
+              columnWidth: '.social-card', //The width of the first card sets the column width (note: all .social-card are the same width)
+              itemSelector: '.social-card',
+              gutter: 0, //Use margins instead. I found that this makes the grid much more consistent
+              isFitWidth: true, 
+              animate: true
     });
+    populateSocialMedia(25, offsetNumber)
 });
 
+//probably doens't actually do anything. But Just a catch to make sure that the grid is reloaded after the images are
 $(window).load(function() {            
     $('#the-studio .content').masonry( 'reloadItems' );
     $('#the-studio .content').masonry( 'layout' );
 });
 
+//Scroll all the way to the bottom and load more events!
 $(function(){
    $(window).scroll(function(){
        if($(document).height()==$(window).scrollTop()+$(window).height()){
