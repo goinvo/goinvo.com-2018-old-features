43d055b0dfdd0af954ccaa06f6ac8bcf3199aafa
Noel Forté <ntforte@gmail.com>
2014-07-24 12:33:10 -0400

    Closes #38 and closes #35

--- source/javascripts/worknav.js
+++ source/javascripts/worknav.js
@@ -15,25 +15,29 @@ $( document ).ready(function() {
 		{
 			"title" : "Affinnova",
 			"image" : "concept-studio-product-blueprint-stickies.jpg"
-		}, 
+		},
 		{
 			"title" : "Dataxu",
 			"image" : "dataxu-campaign-dashboard-user-interface-tablet.jpg"
-		}, 
+		},
 		{
 			"title" : "McAfee",
 			"image" : "mcafee-total-protection-desktop-app-design.jpg"
 		},
 		{
 			"title" : "Microsoft",
-			"image" : "Microsoft-expression-studio-redesign.jpg"
+			"image" : "microsoft-expression-studio-redesign.jpg"
 		},
 		{
 			"title" : "Numera",
 			"image" :  "numera-whiteboard-ui-sketch.jpg"
-		}, 
+		},
 		{
-			"title" : "Ruelala",
+			"title" : "Personal Genome Project",
+			"image" : "openhumans_ecosystem.jpg"
+		},
+		{
+			"title" : "Rue La La",
 			"image" : "ruelala-current-workflow-documentation.jpg"
 		}
 	];
@@ -45,7 +49,7 @@ $( document ).ready(function() {
 
 	var index = 0;
 	var imgURL = "";
-	
+
 	if(current.indexOf("3m") >= 0) {
 		index = 1;
 	}
@@ -61,23 +65,30 @@ $( document ).ready(function() {
 	else if(current.indexOf("mcafee") >= 0) {
 		index = 5;
 	}
-	else if(current.indexOf("numera") >= 0) {
+	else if(current.indexOf("microsoft") >= 0) {
 		index = 6;
 	}
-	else {
-		index = -1; 
+	else if(current.indexOf("numera") >= 0) {
+		index = 7;
 	}
-	
+	else if(current.indexOf("personalgenomeproject") >= 0) {
+		index = 8;
+	}
+	else {
+		index = -1;
+	}
+
 	if(index > 0) {
-		imgURL = "../../images/clients/" + projects[index].title.toLowerCase() + "/" + projects[index].image;  //note the '/v01/' .. this is hardcoded so it works when we upload to the server
-		
+		imgURL = "../../images/clients/" + projects[index].title.replace(/ /g, '').toLowerCase() + "/" + projects[index].image;
+		console.log(imgURL);
+
 		next.css({
 			"background": "linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.5) 100%), url(" + imgURL + ")",
 			"background-size": "cover",
 			"background-position": "center"
 		})
 		next.on("click", function () {
-			   window.location.href =  '../../clients/' + projects[index].title.toLowerCase();
+			   window.location.href =  '../../clients/' + projects[index].title.replace(/ /g, '').toLowerCase();
 		});
 		nextTitle.html(projects[index].title);
 	}
@@ -85,22 +96,22 @@ $( document ).ready(function() {
 		next.on("click", function () {
 			   window.location.href = '../../work/process/';
 		});
-		nextArea.html('Next: See Our Process');
+		nextArea.html('Next: <span id="next">See Our Process</span>');
 		next.css({
 			"background": "linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.5) 100%), url(../../images/process/jpg/intro.jpg)",
 			"background-size": "cover",
 			"background-position": "center 25%"
 		})
 	}
-	
-	
+
+
 	Mousetrap.bind('left', function () {
 		var l = getIndex();
 		if(l >= 0 && l <= 5){
-			l = l -1;   
+			l = l -1;
 		}
 		if(l >= 0 && l <= 5){
-			var url = projects[l].title.toLowerCase(); //note the '/v01/'
+			var url = projects[l].title.replace(/ /g, '').toLowerCase(); //note the '/v01/'
 			window.location.href = url;
 		}
 	});
@@ -108,10 +119,10 @@ $( document ).ready(function() {
 	Mousetrap.bind('right', function () {
 		var l = getIndex();
 		if(l >= 0 && l <= 5){
-			l = l +1;   
+			l = l +1;
 		}
 		if(l >= 0 && l <= 5){
-			var url = projects[l].title.toLowerCase(); //note the '/v01/'
+			var url = projects[l].title.replace(/ /g, '').toLowerCase(); //note the '/v01/'
 			window.location.href = url;
 		}
 	});
@@ -120,31 +131,34 @@ $( document ).ready(function() {
 function getIndex() {
 	var index2 = 0;
 	var current = document.URL;
-	
+
 	if(current.indexOf("3m") >= 0) {
-		index2 = 0;
+		index = 1;
 	}
 	else if(current.indexOf("hgraph") >= 0) {
-		index2 = 1;
+		index = 2;
 	}
 	else if(current.indexOf("affinnova") >= 0) {
-	   index2 = 2;
+		index = 3;
 	}
 	else if(current.indexOf("dataxu") >= 0) {
-		index2 = 3;
+		index = 4;
 	}
 	else if(current.indexOf("mcafee") >= 0) {
-		index2 = 4;
+		index = 5;
+	}
+	else if(current.indexOf("microsoft") >= 0) {
+		index = 6;
 	}
 	else if(current.indexOf("numera") >= 0) {
-		index2 = 5;
+		index = 7;
 	}
-	else if(current.indexOf("ruelala") >= 0) {
-		index2 = 6;
+	else if(current.indexOf("personalgenomeproject") >= 0) {
+		index = 8;
 	}
 	else {
-		index2 = -1; 
+		index2 = -1;
 	}
-	
+
 	return index2;
 }
