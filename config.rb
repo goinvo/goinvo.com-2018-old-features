15dca68be2fdfc04032ccaf7b030a359a114280e
Noel Forté <ntforte@gmail.com>
2014-08-06 11:03:40 -0400

    Initialized removal process.
    WARNING: This code is still far from production ready. The idea is to
    have the CSS not be dependent on Zurb foundation so the site can run
    more efficiently.

--- config.rb
+++ config.rb
@@ -5,8 +5,6 @@
 # Change Compass configuration
 compass_config do |config|
 	# Require any additional compass plugins here.
-	config.add_import_path "bower_components/foundation/scss"
-
 	config.output_style = :compact
 end
 
