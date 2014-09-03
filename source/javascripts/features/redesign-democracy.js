02159d9f9e69dc6b9fe7870e92b25bc5f33f2af2
Noel Forté <ntforte@gmail.com>
2014-09-03 18:01:54 -0400

    v02 Beta

--- source/javascripts/features/redesign-democracy.js
+++ source/javascripts/features/redesign-democracy.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	// Super Global Variable Time
 	var animationSpeed = 400; //(x 1ms)
+	var ableToNavigate = true;
 
 	// Init Governments Slider
 	var govtSlider = $('#govtSlider').slick({
@@ -19,43 +20,62 @@ $(document).ready(function(){
 	//If you click a slider's arrow...
 	$('.slider-arrow').click(function(event) {
 		event.preventDefault();
-		stop();
+		var currentSlideIndex = govtSlider.slickCurrentSlide();
+		$('.govtSlides .visualNav .active').toggleClass('active');
 		if ($(this).hasClass('next')) {
 			govtSlider.slickNext();
+			if (currentSlideIndex === 5) {
+				$('.govtSlides .visualNav .active').toggleClass('active');
+				$('.govtSlides .visualNav .slide0').toggleClass('active');
+			} else {
+				$('.govtSlides .visualNav .slide' + (currentSlideIndex + 1)).toggleClass('active');
+			}
 		} else {
 			govtSlider.slickPrev();
+			if (currentSlideIndex === 0) {
+				$('.govtSlides .visualNav .active').toggleClass('active');
+				$('.govtSlides .visualNav .slide5').toggleClass('active');
+			} else {
+				$('.govtSlides .visualNav .slide' + (currentSlideIndex - 1)).toggleClass('active');
+			}
 		}
+		
 		var currentSlideTotalHeight = $('#govtSlider .slick-active').outerHeight();
 		var currentSlideTextHeight = $('#govtSlider .slick-active .text').outerHeight();
 		$('#govtSlider .slick-list').animate({
 			height: currentSlideTotalHeight + 'px'
-		}, animationSpeed);
+		}, animationSpeed, function(){
+			ableToNavigate = true;
+		});
 		$('#govtSlider .slick-active .image').css({
 			height: currentSlideTextHeight + 'px'
 		});
-
 	});
 
 	// If you click a government slider's buttons...
 	$('.govtSlides .visualNav .slider-button').click(function(event) {
 		//Get to the slide!
 		event.preventDefault();
-		stop();
-		buttonClass = $(this).attr('class');
-		slideNumber = buttonClass.replace( /^\D+/g, '');
-		$('.govtSlides .visualNav .slider-button.active').toggleClass('active');
-		$(this).toggleClass('active');
-		govtSlider.slickGoTo(parseInt(slideNumber));
+		if (ableToNavigate) {
+			ableToNavigate = false;
+			$('#govtSlider').stop();
+			$('#govtSlider .slick-list').stop();
+			buttonClass = $(this).attr('class');
+			slideNumber = buttonClass.replace( /^\D+/g, '');
+			$('.govtSlides .visualNav .slider-button.active').toggleClass('active');
+			$(this).toggleClass('active');
+			govtSlider.slickGoTo(parseInt(slideNumber));
 
-		// While that's happening, let's do something else, like reinit'ing the vars.
-		var currentSlideTotalHeight = $('#govtSlider .slick-active').outerHeight();
-		var currentSlideTextHeight = $('#govtSlider .slick-active .text').outerHeight();
-		$('#govtSlider .slick-list').animate({
-			height: currentSlideTotalHeight + 'px'
-		}, animationSpeed);
-		$('#govtSlider .slick-active .image').animate({
-			height: currentSlideTextHeight + 'px'
-		});
+			// While that's happening, let's do something else, like reinit'ing the vars.
+			var currentSlideTotalHeight = $('#govtSlider .slick-active').outerHeight();
+			var currentSlideTextHeight = $('#govtSlider .slick-active .text').outerHeight();
+			$('#govtSlider .slick-active .image').css('height', currentSlideTextHeight + 'px');
+			$('#govtSlider .slick-list').animate({
+				height: currentSlideTotalHeight + 'px'
+			}, animationSpeed, function(){
+				ableToNavigate = true;
+			});
+		}
 	});
 
 	// Init Voting Slider
@@ -74,24 +94,24 @@ $(document).ready(function(){
 
 	// If you click a voting slider's buttons...
 	$('.votingSlides .visualNav .slider-button').click(function(event) {
-		//Get to the slide!
 		event.preventDefault();
-		stop();
-		buttonClass = $(this).attr('class');
-		slideNumber = buttonClass.replace( /^\D+/g, '');
-		$('.votingSlides .visualNav .slider-button.active').toggleClass('active');
-		$(this).toggleClass('active');
-		votingSlider.slickGoTo(parseInt(slideNumber));
+		if (ableToNavigate) {
+			ableToNavigate = false;
+			buttonClass = $(this).attr('class');
+			slideNumber = buttonClass.replace( /^\D+/g, '');
+			$('.votingSlides .visualNav .slider-button.active').toggleClass('active');
+			$(this).toggleClass('active');
+			votingSlider.slickGoTo(parseInt(slideNumber));
 
-		// While that's happening, let's do something else, like reinit'ing the vars.
-		var currentSlideTotalHeight = $('#votingSlider .slick-active').outerHeight();
-		var currentSlideTextHeight = $('#votingSlider .slick-active .text').outerHeight();
-		$('#votingSlider .slick-list').animate({
-			height: currentSlideTotalHeight + 'px'
-		}, animationSpeed);
-		$('#votingSlider .slick-active .image').animate({
-			height: currentSlideTextHeight + 'px'
-		});
+			// While that's happening, let's do something else, like reinit'ing the vars.
+			var currentSlideTotalHeight = $('#votingSlider .slick-active').outerHeight();
+			var currentSlideTextHeight = $('#votingSlider .slick-active .text').outerHeight();
+			$('#votingSlider .slick-list').animate({
+				'height': currentSlideTotalHeight + 'px'
+			}, animationSpeed, function(){
+				ableToNavigate = true;
+			});
+		}
 	});
 });
 
