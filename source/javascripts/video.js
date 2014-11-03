986a25fee109a39cd978b57d2bafc4723d3c154a
Adam Pere <adampere@Adams-MacBook-Pro-2.local>
2014-11-03 15:39:08 -0500

    healthcare page video seemingly complete/mobile and large screen ready
    (the rest of the page isn’t……..)

--- source/javascripts/video.js
+++ source/javascripts/video.js
@@ -1,6 +1,10 @@
 // This js file sets up the ability to have a clickable image play a video.
 var videos = {
-	"res-proj" : '<iframe src="https://embed-ssl.ted.com/talks/stephen_friend_the_hunt_for_unexpected_genetic_heroes.html?autoplay=1" width="854" height="480" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
+	"res-proj" : {
+		'source' : '<iframe src="https://embed-ssl.ted.com/talks/stephen_friend_the_hunt_for_unexpected_genetic_heroes.html?autoplay=1" width="854" height="480" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
+		'width' : 854,
+		'height' : 400
+	}
 };
 
 $( document ).ready(function() {	
@@ -11,12 +15,36 @@ $( document ).ready(function() {
 		parentContainer = $(playT.parents('.hero-video-container')[0]);
 		var videoPlayer = $(parentContainer.children('.video-player')[0]);
 		var videoBanner = $(parentContainer.children('.video-banner')[0]);
+		var videoBackground = $(videoBanner.children('.video-background')[0]);
+		var videoButtons = $(videoBanner.children('.video-control-button')[0]);
+
+		videoPlayer.html(videos[videoName]['source']);
+		var videoFrame = $(videoPlayer.children('iframe')[0]);
+		var sWidth = $(window).width();
 		
-		videoBanner.css('opacity', 0);
+		if(sWidth >= 760) {
+			videoBanner.css('opacity', 0);
+		}
 		
-		videoPlayer.html(videos[videoName]);
+		
+		if(sWidth <= 894) {
+			rw = videos[videoName]['width'];
+			rh = videos[videoName]['height'];
+			newWidth = sWidth - 40;
+			newHeight = (rh * newWidth)/rw;
+			console.log(newHeight);
+			videoFrame.attr('width', newWidth + 'px');
+			videoFrame.attr('height', newHeight + 'px');
+		}
+		videoPlayer.fitVids();
 		setTimeout(function() {
-			videoBanner.css('display', 'none');
+			if(sWidth >= 760){
+				videoBanner.css('display', 'none');
+			}
+			else {
+				videoBackground.css('display', 'none');	
+				videoButtons.css('display', 'none');
+			}
 			videoPlayer.css('display', 'block');
 		}, 300);
 		
