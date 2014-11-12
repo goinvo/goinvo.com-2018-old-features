e0fb1b8393ffc06a286e22cdf79e54370e180f77
Adam Pere <adampere@Adams-MacBook-Pro-4.local>
2014-11-12 12:28:34 -0500

    finished all sections, styling is complete or at the final testing phase
    also added js for highlighting the specific section in the nav bar….
    code is written so that it could potentially work with other pages

--- source/javascripts/featurenav-scroll.js
+++ source/javascripts/featurenav-scroll.js
@@ -14,4 +14,27 @@ $(window).on('scroll', function () {
 	} else {
 		fixed.css('display', 'none');
 	}
+	
+	
+	//nav section highlighting (toggling class .active)		
+	var sections = [$('header.one'), $('header.two'), $('header.three'), $('header.four'), $('header.five')];
+	var navSections = [$('.main-nav a.one'), $('.main-nav a.two'), $('.main-nav a.three'), $('.main-nav a.four'), $('.main-nav a.five')];
+	
+	for(var i = 0; i < sections.length; i++) {
+		myPosition = sections[i].position().top;
+		
+		if (i == sections.length-1) {
+			if(scrollTop >= myPosition && !navSections[i].hasClass('active')) {
+				$('.main-nav .active').toggleClass('active', false);	
+				navSections[i].toggleClass('active', true);
+			}
+		}
+		else{
+			nextPosition = sections[i+1].position().top;
+			if(scrollTop >= myPosition && scrollTop < nextPosition && !navSections[i].hasClass('active')) {
+				$('.main-nav .active').toggleClass('active', false);	
+				navSections[i].toggleClass('active', true);
+			}
+		}
+	}
 });
