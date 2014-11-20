b75e095b0ddf6dcb13f20c9c2c2b67440c80050d
Adam Pere <adampere@Adams-MacBook-Pro-4.local>
2014-11-20 18:03:14 -0500

    slideshow, homepage update, healthcare pages + client data added
    Tabeeb + Partners GeneInsight are both empty (the content in them
    belongs to Health Axioms)

--- source/javascripts/invo-time-feature.js
+++ source/javascripts/invo-time-feature.js
@@ -9,9 +9,9 @@ $( window ).resize(function() {
 
 $(function() {
     $(".rslides").responsiveSlides({
-  auto: false,             // Boolean: Animate automatically, true or false
+  auto: true,             // Boolean: Animate automatically, true or false
   speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  timeout: 4500,          // Integer: Time between slide transitions, in milliseconds
   pager: false,           // Boolean: Show pager, true or false
   nav: true,             // Boolean: Show navigation, true or false
   random: true,          // Boolean: Randomize the order of the slides, true or false
