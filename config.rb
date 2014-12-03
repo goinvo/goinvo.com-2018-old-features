a67dcce27741fb08960bafe89194414ec7f7d9aa
Adam Pere <adampere@Adams-MacBook-Pro-4.local>
2014-12-03 18:58:36 -0500

    video changes

--- config.rb
+++ config.rb
@@ -50,7 +50,7 @@ activate :cache_buster
 
 # S3 Sync
 activate :s3_sync do |s3_sync|
-	s3_sync.bucket                     = 'www.goinvo.com' # The name of the S3 bucket you are targetting. This is globally unique.
+	s3_sync.bucket                     = 'staging.goinvo.com' # The name of the S3 bucket you are targetting. This is globally unique.
 	s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
 	s3_sync.delete                     = true # We delete stray files by default.
 	s3_sync.after_build                = false # We do not chain after the build step by default.
