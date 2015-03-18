2c24c513e41e62527dc62f059b0b728e3a3318b7
craigmcginley <craigmcginley@gmail.com>
2015-03-18 19:08:34 -0400

    getting closer to proper fading

--- source/javascripts/features/disrupt.js
+++ source/javascripts/features/disrupt.js
@@ -76,6 +76,15 @@ $(document).ready(function(event){
   var slideshowOpen = false;
   var bottomNavState = 'closed';
 
+  var windowHeight = $(window).height();
+  var documentHeight = $(document).height();
+
+  // Vars for scroll fades
+  var firstVideo = $('.header-contents').first();
+  var secondVideo = $('.sec-header').first();
+  var firstVideoBottom = firstVideo.offset().top + firstVideo.height();
+  var secondVideoTop = secondVideo.offset().top;
+
   // ===== Initialization =====
   $("video").each(function() {
     this.volume = 0.5;
@@ -102,6 +111,12 @@ $(document).ready(function(event){
 
   // ===== Resize event =====
   $(window).resize(function() {
+    // Recalc for scroll fades
+    windowHeight = $(window).height();
+    documentHeight = $(document).height();
+    firstVideoBottom = firstVideo.offset().top + firstVideo.height();
+    secondVideoTop = secondVideo.offset().top;
+
     layout(panels, grid, gridArea, slideshowContainer);
     var gridHeight = grid.height();
     gridArea.css({height: gridHeight });
@@ -126,48 +141,76 @@ $(document).ready(function(event){
     }
   });
 
-
-  var bottomVid = 6000;
   // ===== Scroll event =====
   $(window).scroll(function() {
-    var winST = $(window).scrollTop();
-    var winHeight = $(window).height();
-    var docHeight = $(document).height();
+    var scrollTop = $(window).scrollTop();
+    var windowBottom = scrollTop + windowHeight;
+    var firstVideoCalc = ((firstVideoBottom - scrollTop) / firstVideoBottom);
+    var secondVideoCalc = ((windowBottom-secondVideoTop) / (documentHeight - secondVideoTop));
 
     // Bottom nav animation
-    if (winST + winHeight > docHeight - 50) {
-      if (bottomNavState === 'closed') {
-        $('#bottom-nav').animate({
-          opacity: 1,
-          bottom: "50px"
-        }, {
-          duration: 500,
-          complete: function() {
-            bottomNavState = 'open';
-            $(this).css('pointer-events', 'auto');
-          }
-        });
+    // if (winST + winHeight > docHeight - 50) {
+    //   if (bottomNavState === 'closed') {
+    //     $('#bottom-nav').animate({
+    //       opacity: 1,
+    //       bottom: "50px"
+    //     }, {
+    //       duration: 500,
+    //       complete: function() {
+    //         bottomNavState = 'open';
+    //         $(this).css('pointer-events', 'auto');
+    //       }
+    //     });
+    //   }
+    // }
+
+    // if (winST + winHeight < docHeight - 100) {
+    //   $('#bottom-nav').animate({
+    //     opacity: 0,
+    //     bottom: "0px"
+    //   }, {
+    //     duration: 0,
+    //     complete: function() {
+    //       bottomNavState = 'closed';
+    //       $(this).css('pointer-events', 'none');
+    //     }
+    //   });
+    // }
+
+    // Fade videos
+    if ( firstVideoCalc >= 0 ) {
+      firstVideo.css({'opacity': firstVideoCalc });
+    }
+
+    if ( secondVideoCalc >= 0 ) {
+      secondVideo.css({'opacity': secondVideoCalc });
+    }
+  });
+
+  // Color scroll fading
+  var colors = [
+    {top: '#1c97aa', bottom: '#e88c35'}
+  ];
+
+  $('.container.content').colorScroll({
+    colors: [
+      {
+        color: colors[page].top,
+        position: '0'
+      },
+      {
+        color: '#ffffff',
+        position: firstVideoBottom + (firstVideoBottom*0.2)
+      },
+      {
+        color: '#ffffff',
+        position: secondVideoTop - (windowHeight - (windowHeight*0.05))
+      },
+      {
+        color: colors[page].bottom,
+        position: documentHeight - windowHeight
       }
-    }
-
-    if (winST + winHeight < docHeight - 100) {
-      $('#bottom-nav').animate({
-        opacity: 0,
-        bottom: "0px"
-      }, {
-        duration: 0,
-        complete: function() {
-          bottomNavState = 'closed';
-          $(this).css('pointer-events', 'none');
-        }
-      });
-    }
-
-    // Fade video
-    $('.header-contents').first().css({'opacity': ( 400 - winST ) / 400 });
-
-    var secVidTop = $('.sec-header').offset().top;
-    $('.sec-header').first().css({'opacity': (0 + ((winST + winHeight - secVidTop - 400) / 200)) });
+    ]
   });
 
   // ===== Grid panels hover states =====
