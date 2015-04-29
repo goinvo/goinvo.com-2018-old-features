d36ece49e4fe73ae799f78f3eb7da9286ed7db8b
craigmcginley <craigmcginley@gmail.com>
2015-04-29 12:50:02 -0400

    trying out new video implementation

--- source/javascripts/features/disrupt.js
+++ source/javascripts/features/disrupt.js
@@ -3,26 +3,26 @@ $(document).ready(function(event){
   var documentHeight = $(document).height();
   var siteNav = $('#site-overlay');
   var articleNav = $('#article-nav');
+  var navOffset = siteNav.height() + articleNav.height();
   var mobileMenu = articleNav.find('.mobile-menu');
   var bottomNav = $('#bottom-nav');
   var siteFooter = $('#main-footer');
-  var firstVideo = $('#top');
-  var secondVideo = $('#bottom');
-  var firstTitle = firstVideo.find('h1'); // H1 only exists on first page of article
+  var topFade = $('#top');
+  var bottomFade = $('#bottom');
+  var firstVideo = $('.top-vid');
+  var secondVideo = $('.bottom-vid');
+  var videosAreGo = false;
+  var firstTitle = topFade.find('h1'); // H1 only exists on first page of article
   var scrollTop = $(window).scrollTop();
   var windowBottom = scrollTop + windowHeight;
-  var firstVideoBottom = firstVideo.offset().top + firstVideo.height();
-  var secondVideoTop = secondVideo.offset().top;
-  var secondVideoBottom = secondVideoTop + secondVideo.height();
-  var navOffset = siteNav.height();
+  var topFadeBottom = topFade.offset().top + topFade.height();
+  var bottomFadeTop = bottomFade.offset().top;
+  var bottomFadeBottom = bottomFadeTop + bottomFade.height();
   var mainBackground = $('.container.content');
   var articleContent = $('.disrupt');
   var socialButtons = $('.social-container');
-  var vid1 = document.getElementsByClassName('top-vid')[0].getElementsByTagName('video')[0];
-  var vid2 = document.getElementsByClassName('bottom-vid')[0].getElementsByTagName('video')[0];
-  var vidsToLoad = 2;
-  var firstVideoCalc = ((firstVideoBottom - scrollTop) / firstVideoBottom);
-  var secondVideoCalc = ((windowBottom-secondVideoTop) / (documentHeight - secondVideoTop));
+  var topFadeCalc = ((topFadeBottom - scrollTop) / topFadeBottom);
+  var bottomFadeCalc = ((windowBottom-bottomFadeTop) / (documentHeight - bottomFadeTop));
   var colors = [
     {top: '#0282C1', bottom: '#E68B35'},
     {top: '#E68B35', bottom: '#DD2E64'},
@@ -47,21 +47,19 @@ $(document).ready(function(event){
 
   // Add margin to top video equal to total nav heights
   function topVidMargin() {
-    navOffset = siteNav.height();
-    if (!articleNav.hasClass('mobile')) {
-      navOffset += articleNav.height();
-    }
+    navOffset = siteNav.height() + articleNav.height();
   }
 
   // If all the videos are loaded, set up the color scrolling
   function videosLoaded() {
     vidsToLoad -= 1;
     if (vidsToLoad <= 0) {
-      firstVideoBottom = firstVideo.offset().top + firstVideo.height();
-      secondVideoTop = secondVideo.offset().top;
-      secondVideoBottom = secondVideoTop + secondVideo.height();
+      topFadeBottom = topFade.offset().top + topFade.height();
+      bottomFadeTop = bottomFade.offset().top;
+      bottomFadeBottom = bottomFadeTop + bottomFade.height();
       documentHeight = $(document).height();
-
+      topVidMargin();
+      topFade.css("margin-top", navOffset);
       mainBackground.colorScroll({
         colors: [
           {
@@ -70,15 +68,15 @@ $(document).ready(function(event){
           },
           {
             color: '#ffffff',
-            position: firstVideoBottom
+            position: topFadeBottom
           },
           {
             color: '#ffffff',
-            position: secondVideoTop - windowHeight
+            position: bottomFadeTop - windowHeight
           },
           {
             color: colors[page].bottom,
-            position: secondVideoBottom - windowHeight
+            position: bottomFadeBottom - windowHeight
           }
         ]
       });
@@ -87,20 +85,6 @@ $(document).ready(function(event){
 
 
   // ===== Initialization =====
-  try {
-    Typekit.load({
-      active: function() {
-        topVidMargin();
-        firstVideo.css("margin-top", navOffset);
-
-        // Only applicable for section-4, but need the second video height from this script
-        $('#grid-section').css("margin-bottom", (windowHeight - secondVideo.height()));
-      }
-    });
-  } catch(e) {
-    console.log(e);
-  }
-
   mainBackground.css("background-color", colors[page].top);
   articleContent.animate({
     "opacity": 1
@@ -108,32 +92,49 @@ $(document).ready(function(event){
   socialButtons.hide();
   bottomNav.find('.title').css("color", colors[page].bottom);
 
-  if ($(window).width() < 800 || !videoSupport()) {
-    $(".video-container").css("display", "none");
-    $(".image-container").css("display", "block");
+  if ($(window).width() > 800) {
+    if (videoSupport()) {
+      videosAreGo = true;
+      var section = page + 1;
+      firstVideo.find('.placeholder').remove();
+      firstVideo.find('.video-container').prepend('<video autoplay="true" muted="true" loop="true" poster="../../images/features/disrupt/video_posters/section-' + section + '-top.jpg"><source src="../../videos/disrupt/section-' + section + '-header.mp4" type="video/mp4"><source src="../../videos/disrupt/section-' + section + '-header.webm" type="video/webm"></video>');
+      secondVideo.find('.placeholder').remove();
+      secondVideo.find('.video-container').prepend('<video autoplay="true" muted="true" loop="true" poster="../../images/features/disrupt/video_posters/section-' + section + '-bottom.jpg"><source src="../../videos/disrupt/section-' + section + '-footer.mp4" type="video/mp4"><source src="../../videos/disrupt/section-' + section + '-footer.webm" type="video/webm"></video>');
+      var vid1 = document.getElementsByClassName('top-vid')[0].getElementsByTagName('video')[0];
+      var vid2 = document.getElementsByClassName('bottom-vid')[0].getElementsByTagName('video')[0];
+      var vidsToLoad = 2;
+      vid1.onloadedmetadata = function() {
+        videosLoaded();
+      }
+      vid2.onloadedmetadata = function() {
+        videosLoaded();
+      }
+      vid1.oncanplay = function() {
+        firstTitle.delay(2000).animate({
+          "letter-spacing" : "0.7em",
+          "opacity" : "1"
+        }, {
+          duration: 12000
+        });
+        socialButtons.delay(6000).fadeIn(6000);
+      }
+    } else {
+      topFadeBottom = topFade.offset().top + topFade.height();
+      bottomFadeTop = bottomFade.offset().top;
+      bottomFadeBottom = bottomFadeTop + bottomFade.height();
+      documentHeight = $(document).height();
+      firstTitle.delay(2000).animate({
+        "letter-spacing" : "0.7em",
+        "opacity" : "1"
+      }, {
+        duration: 12000
+      });
+      socialButtons.delay(6000).fadeIn(6000);
+      mainBackground.css("background-color", "white");
+    }
+  } else {
     articleNav.addClass("mobile");
-  }
-
-  mobileMenu.on("click", function() {
-    $(this).parent().find('ol').toggleClass('open');
-    articleNav.find('.toggle-arrow').toggleClass('open');
-  });
-  // Close article nav if site nav is opened
-  $('#mobile-hamburger').on("click", function() {
-    articleNav.find('ol').removeClass('open');
-  });
-
-  // Once we have just the dimensions of the videos, we can check this function
-  vid1.onloadedmetadata = function() {
-    videosLoaded();
-  }
-  vid2.onloadedmetadata = function() {
-    videosLoaded();
-  }
-
-  // Lets get the first title animations rolling ASAP
-  if ($(window).width() > 800 && videoSupport()) {
-    vid1.oncanplay = function() {
+    if (page === 0) {
       firstTitle.delay(2000).animate({
         "letter-spacing" : "0.7em",
         "opacity" : "1"
@@ -142,16 +143,61 @@ $(document).ready(function(event){
       });
       socialButtons.delay(6000).fadeIn(6000);
     }
-  } else {
-    firstTitle.delay(2000).animate({
-      "letter-spacing" : "0.7em",
-      "opacity" : "1"
-    }, {
-      duration: 12000
+    topVidMargin();
+    topFade.css("margin-top", navOffset);
+    topFadeBottom = topFade.offset().top + topFade.height();
+    bottomFadeTop = bottomFade.offset().top;
+    bottomFadeBottom = bottomFadeTop + bottomFade.height();
+    documentHeight = $(document).height();
+    mainBackground.colorScroll({
+      colors: [
+        {
+          color: colors[page].top,
+          position: '0'
+        },
+        {
+          color: '#ffffff',
+          position: topFadeBottom
+        },
+        {
+          color: '#ffffff',
+          position: bottomFadeTop - windowHeight
+        },
+        {
+          color: colors[page].bottom,
+          position: bottomFadeBottom - windowHeight
+        }
+      ]
     });
-    socialButtons.delay(6000).fadeIn(6000);
-    mainBackground.css("background-color", "white");
   }
+
+
+  try {
+    Typekit.load({
+      active: function() {
+        topVidMargin();
+        topFade.css("margin-top", navOffset);
+        topFadeBottom = topFade.offset().top + topFade.height();
+        bottomFadeTop = bottomFade.offset().top;
+        bottomFadeBottom = bottomFadeTop + bottomFade.height();
+        documentHeight = $(document).height();
+        // Only applicable for section-4, but need the second video height from this script
+        $('#grid-section').css("margin-bottom", (windowHeight - bottomFade.height()));
+      }
+    });
+  } catch(e) {
+    console.log(e);
+  }
+
+  // Mobile navigation setup
+  mobileMenu.on("click", function() {
+    $(this).parent().find('ol').toggleClass('open');
+    articleNav.find('.toggle-arrow').toggleClass('open');
+  });
+  // Close article nav if site nav is opened
+  $('#mobile-hamburger').on("click", function() {
+    articleNav.find('ol').removeClass('open');
+  });
 
 
   // ===== Resize event =====
@@ -163,14 +209,14 @@ $(document).ready(function(event){
       articleNav.removeClass("mobile");
     }
     topVidMargin();
-    firstVideo.css("margin-top", navOffset);
+    topFade.css("margin-top", navOffset);
 
     // Recalc for scroll fades
     windowHeight = $(window).height();
     documentHeight = $(document).height();
-    firstVideoBottom = firstVideo.offset().top + firstVideo.height();
-    secondVideoTop = secondVideo.offset().top;
-    secondVideoBottom = secondVideoTop + secondVideo.height();
+    topFadeBottom = topFade.offset().top + topFade.height();
+    bottomFadeTop = bottomFade.offset().top;
+    bottomFadeBottom = bottomFadeTop + bottomFade.height();
   });
 
 
@@ -178,16 +224,18 @@ $(document).ready(function(event){
   $(window).scroll(function() {
     scrollTop = $(window).scrollTop();
     windowBottom = scrollTop + windowHeight;
-    firstVideoCalc = ((firstVideoBottom - scrollTop) / firstVideoBottom);
-    secondVideoCalc = ((windowBottom-secondVideoTop) / (documentHeight - secondVideoTop));
+    topFadeCalc = ((topFadeBottom - scrollTop) / topFadeBottom);
+    bottomFadeCalc = ((windowBottom-bottomFadeTop) / (documentHeight - bottomFadeTop));
 
     // Let's play the second video here
-    if (windowBottom >= documentHeight / 2) {
-      vid2.play();
+    if (videosAreGo) {
+      if (windowBottom >= documentHeight / 2) {
+        vid2.play();
+      }
     }
 
     //Bottom nav animation
-    if (windowBottom > secondVideoBottom) {
+    if (windowBottom > bottomFadeBottom) {
       bottomNav.delay(150).animate({
         opacity: "1",
         bottom: siteFooter.outerHeight()
@@ -200,15 +248,15 @@ $(document).ready(function(event){
     }
 
     // Fade videos
-    if ( firstVideoCalc >= 0 ) {
-      firstVideo.css({'opacity': firstVideoCalc });
+    if ( topFadeCalc >= 0 ) {
+      topFade.css({'opacity': topFadeCalc });
     }
-    if ( secondVideoCalc >= 0 ) {
+    if ( bottomFadeCalc >= 0 ) {
       var extra = 0;
       if (page === 5) {
         extra = 0.4;
       }
-      secondVideo.css({'opacity': secondVideoCalc + extra });
+      bottomFade.css({'opacity': bottomFadeCalc + extra });
     }
   });
 });
