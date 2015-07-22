41e3d39d09cd238e02df0a087c73147f5914eb5b
Ben Salinas <ben.salinas@gmail.com>
2015-07-22 16:48:52 -0400

    coloring the sankey

--- source/javascripts/features/us-healthcare/index.js
+++ source/javascripts/features/us-healthcare/index.js
@@ -40,12 +40,34 @@ $(document).ready(function(){
     $(this).addClass('selected');
   });
 
+
+  var followLinks = function myself(find_targets, nodeKey){
+    var data_attribute = "data-key-source";
+    var other = 'data-key-target';
+    if(find_targets){ 
+      data_attribute = "data-key-target";
+      other = 'data-key-source';
+   } 
+    $('.link['+data_attribute+'="'+nodeKey+'"]').each(function(){
+      $(this).attr('data-highlight', find_targets?'backwards':'forwards');
+        myself(find_targets,$(this).attr(other));
+    });
+  };
   $('.individual-action').click(function(e){
     $('.link').attr('data-highlight','false');
     $('.individual-action').removeClass('selected');
     var this_key = $(this).attr('data-key');
     console.log(this_key)
-    $('.link[data-key-source="'+this_key+'"]').attr('data-highlight', 'true');
+    //First, find things that use this key as a source.
+    followLinks(false, this_key);
+    //Then go the other way.
+    followLinks(true, this_key);
+    // $('.link[data-key-source="'+this_key+'"]').each(function(){
+    //   $(this).attr('data-highlight', 'true');
+    //   //First, find the thing that has a source of this key
+    //   followLinks(false, $(this).attr('data-key-target'));
+    //   // followLinks(false, $(this).attr('data-key-source'));
+    // })
     $(this).addClass('selected');
   });
   
