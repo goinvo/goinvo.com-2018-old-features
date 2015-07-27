098b343c0485e945f7db5a91a0293d25428a539c
cvlu013 <cecilelu@mit.edu>
2015-07-27 12:35:38 -0400

    responsive map!!!

--- source/javascripts/features/us-healthcare/cost_comparison_boston.js
+++ source/javascripts/features/us-healthcare/cost_comparison_boston.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
 var myWindow = d3.select(window);
+var w = document.documentElement.clientWidth * .7;
+var whRatio = 5/9.6;
+var h = whRatio * w;
 
-var elementW = window.innerWidth;
-var elementH = window.innerHeight;
 var margin = {top: 20, right: 0, bottom: 50, left: 10},
-    width = .7*elementW,
-    height = 5/9.6 * width;
+    width = w - margin.left-margin.right,
+    height = h;
                                                                          
 var svg = d3.select( "#cost-comparison-boston-chart" )
   .append( "svg" )
@@ -20,7 +21,7 @@ var tooltip = d3.select("#cost-comparison-boston-chart").append("div")
 var g = svg.append( "g" );
 
 var albersProjection = d3.geo.albers()
-  .scale( 190000 )
+  .scale( width * 215 )
   .rotate( [71.057,0] )
   .center( [0, 42.313] )
   .translate( [width/2,height/2] );
@@ -37,9 +38,9 @@ d3.json("/features/us-healthcare/data/neighborhoods.json", function(error, neigh
     .data(neighborhoods.features)
     .enter()
     .append("path")
-    .attr("fill", "#ccc")
+    .attr("fill", "#E0E0E0")
     .attr("stroke", "white")
-    .attr("stroke-width", "0.5px")
+    .attr("stroke-width", "0.75px")
     .attr("d", geoPath);
 
       d3.csv("/features/us-healthcare/data/boston_hospitals.csv", function(data){
@@ -66,7 +67,7 @@ d3.json("/features/us-healthcare/data/neighborhoods.json", function(error, neigh
               tooltip.transition()
                 .duration(200)
                 .style("opacity",1)
-              tooltip.html("<b>" + d.name + "</b>" + "<br/>" + d3.format("$,")(d.price))
+              tooltip.html("<b>" + d.name + "</b>" + "<br/>" + "Echocardiogram Price: " + d3.format("$,")(d.price))
                 .style("left", (d3.event.pageX + 5) + "px")
                 .style("top", (d3.event.pageY + 5) + "px");
             })
@@ -82,4 +83,27 @@ d3.json("/features/us-healthcare/data/neighborhoods.json", function(error, neigh
       });
   
 });
+    
+var initializeSizes = function(){
+    w =  document.documentElement.clientWidth * .7;
+    width = w - margin.left - margin.right;
+    height = whRatio * w;
+
+    svg.attr("width", width)
+       .attr("height", height);
+    
+    albersProjection.translate([width/2,height/2])
+        .scale(width * 215);
+    
+    g.selectAll("path").attr("d", geoPath);
+    
+    hospitals.attr("cx", function(d){
+              return albersProjection([d.lon, d.lat])[0];
+            })
+            .attr("cy", function(d){
+              return albersProjection([d.lon, d.lat])[1];
+            })
+}
+
+myWindow.on('resize.cost', initializeSizes );
 });
