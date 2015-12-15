41fbf327b6f9eafc77edbcf16ea945f9b412db31
cmcgorrill <cmm5215@rit.edu>
2015-12-15 12:29:29 -0500

    universal conditions working in diagram, plus care team additions for each condition

--- source/javascripts/features/careplans/checkboxes.js
+++ source/javascripts/features/careplans/checkboxes.js
@@ -3,11 +3,14 @@ $('input[type="checkbox"]').change(function() {
     if (this.checked){
 	    $.getJSON("/features/careplans/data/conditions.json", function (data) {
 	    	var condition = data[0][id];
+	    	if( $('.care-team > .'+id).length == 0 ){
+		    	$('.care-team').append("<span class="+id+">"+condition["care team"]+"</span>");
+		    }
 	    	$.each(condition["care plan"], function(component, content) {
 	    		component = component.replace(/\s/g, '');
 	    		$.each(content, function(key, val){
-	    			if (val != "") {
-	    				$("#"+component+" > #"+key).append("<span class="+id+">"+val+"</span>");
+	    			if (val != "" && $('#'+component+' > #'+key+' > .'+id).length == 0) {
+	    				$("#"+component+" > #"+key).append("<p class="+id+">"+val+"</p>");
 	    			}
 	    		});
 	    	});
