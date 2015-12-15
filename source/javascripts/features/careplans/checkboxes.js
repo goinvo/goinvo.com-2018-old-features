bc4a9c1ef8436303ca1d05de553ec3fee003d41b
cmcgorrill <cmm5215@rit.edu>
2015-12-15 13:17:51 -0500

    all conditions working, only appending to active pane, saves selection between patients

--- source/javascripts/features/careplans/checkboxes.js
+++ source/javascripts/features/careplans/checkboxes.js
@@ -1,5 +1,8 @@
 $('input[type="checkbox"]').change(function() {
     var id = $(this).attr("id");
+    if (id == "option0" || id == "option1"){
+    	id = id+$(".tab-pane.active")[0].id;
+    }
     if (this.checked){
 	    addCondition(id);
 	}
@@ -18,8 +21,8 @@ function addCondition (id) {
 	    	$.each(condition["care plan"], function(component, content) {
 	    		component = component.replace(/\s/g, '');
 	    		$.each(content, function(key, val){
-	    			if (val != "" && $('#'+component+' > #'+key+' > .'+id).length == 0) {
-	    				$("#"+component+" > #"+key).append("<p class="+id+">"+val+"</p>");
+	    			if (val != "" && $(".tab-pane.active").find('#'+component+' > #'+key+' > .'+id).length == 0) {
+	    				$(".tab-pane.active").find("#"+component+" > #"+key).append("<p class="+id+">"+val+"</p>");
 	    			}
 	    		});
 	    	});
