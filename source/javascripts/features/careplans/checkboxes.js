4671cf078b54cd39156fc0aa22b3d2c1674f174a
cmcgorrill <cmm5215@rit.edu>
2015-12-15 14:33:52 -0500

    fix donna not responding to conditions

--- source/javascripts/features/careplans/checkboxes.js
+++ source/javascripts/features/careplans/checkboxes.js
@@ -1,15 +1,17 @@
-$('input[type="checkbox"]').change(function() {
-    var id = $(this).attr("id");
-    if (id == "option0" || id == "option1"){
-    	id = id+$(".tab-pane.active")[0].id;
-    }
-    if (this.checked){
-	    addCondition(id);
-	}
-	else {
-		$("."+id).remove();
-	}
-});
+$(document).ready(function(){
+	$('input[type="checkbox"]').change(function() {
+	    var id = $(this).attr("id");
+	    if (id == "option0" || id == "option1"){
+	    	id = id+$(".tab-pane.active")[0].id;
+	    }
+	    if (this.checked){
+		    addCondition(id);
+		}
+		else {
+			$(".tab-pane.active").find("."+id).remove();
+		}
+	});
+})
 
 function addCondition (id) {
 	$.getJSON("/features/careplans/data/conditions.json", function (data) {
@@ -28,4 +30,17 @@ function addCondition (id) {
 	    	});
 	    }
     });
+}
+
+function enableDisable () {
+	var boxes = $(".tab-pane.active").find('input[type="checkbox"]');
+	var checked_boxes = $(".tab-pane.active").find('input[type="checkbox"]:checked');
+    if ( checked_boxes.length == 2 ) {
+    	$.each( boxes , function (i, box) {
+    		$(box).prop("disabled", !this.checked);
+    	});
+    }
+    else if ( checked_boxes.length < 2 ){
+    	$(boxes).prop("disabled", false);
+    } 
 }
