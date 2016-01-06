178c5eeeac15562b13e6eeb64b360a85d1cbd3a3
YvonneChow <naruto3135@gmail.com>
2016-01-06 12:10:48 -0500

    fix issues
    1. set fixed height of carousel
    2. add dark cover on title background
    3. change font size
    4. remove underline of arrow on carousel
    5. add: click on a popup and the rest will fade out

--- source/javascripts/features/careplans/case_studies.js
+++ source/javascripts/features/careplans/case_studies.js
@@ -1,59 +1,28 @@
-var nursing = $('#nursing_popup');
-$('#nursing_span').click(function(){
-	$(this).toggleClass('selected_span');
-	if($(nursing).is(':visible')){
-		nursing.fadeOut(100);
+$('.clickable_span').click(function(){
+	$(".popup_content").fadeOut(100);
+	$(".clickable_span").removeClass('selected_span');
+	if($(this).find(".popup_content").is(':visible')){
+		$(this).find(".popup_content").fadeOut(100);
 	} else {
-		nursing.fadeIn(100);
+		$(this).find(".popup_content").fadeIn(100);
+		$(this).addClass('selected_span');
 	}
 });
 
-var life_care_planning = $('#life_care_planning_popup');
-$('#life_care_planning_span').click(function(){
-	$(this).toggleClass('selected_span');
-	if($(life_care_planning).is(':visible')){
-		life_care_planning.fadeOut(100);
-	} else {
-		life_care_planning.fadeIn(100);
+function setCarouselHeight(){
+	var tallest = 0;
+	for(var i = 1;i<=$(".item").length;i++) {
+		if(tallest < $(".item:nth-child("+i+")").height())
+			tallest = $(".item:nth-child("+i+")").height();
 	}
+	return tallest+100;
+}
+
+$(window).resize(function(event) {
+	/* Act on the event */
+	$(".carousel-inner").height(setCarouselHeight());
 });
 
-var EAB = $('#EAB_popup');
-$('#EAB_span').click(function(){
-	$(this).toggleClass('selected_span');
-	if($(EAB).is(':visible')){
-		EAB.fadeOut(100);
-	} else {
-		EAB.fadeIn(100);
-	}
-});
-
-var developmental_psychology = $('#developmental_psychology_popup');
-$('#developmental_psychology_span').click(function(){
-	$(this).toggleClass('selected_span');
-	if($(developmental_psychology).is(':visible')){
-		developmental_psychology.fadeOut(100);
-	} else {
-		developmental_psychology.fadeIn(100);
-	}
-});
-
-var case_management = $('#case_management_popup');
-$('#case_management_span').click(function(){
-	$(this).toggleClass('selected_span');
-	if($(case_management).is(':visible')){
-		case_management.fadeOut(100);
-	} else {
-		case_management.fadeIn(100);
-	}
-});
-
-var litigation = $('#litigation_popup');
-$('#litigation_span').click(function(){
-	$(this).toggleClass('selected_span');
-	if($(litigation).is(':visible')){
-		litigation.fadeOut(100);
-	} else {
-		litigation.fadeIn(100);
-	}
+$(document).ready(function(){
+	$(".carousel-inner").height(setCarouselHeight());
 });
