34870522b2e0b5687571a3bcd978b2496695dafa
craigmcginley <craigmcginley@gmail.com>
2016-01-14 16:02:12 -0500

    feature articles on article page now using yaml data

--- lib/data_helper.rb
+++ lib/data_helper.rb
@@ -2,18 +2,25 @@ module DataHelper
   def client_for_name(client_name)
     match = data.clients.detect { |c| c.name == client_name }
     puts "didn't find a match for name: #{client_name}" if match.nil?
-    
+
     match
   end
-	
+
   def page_for_meta(page)
     match = data.meta_tags.detect { |p| p.page == page }
     puts "didn't find a match for name: #{page}" if match.nil?
 
     match
   end
-  
+
   def practice_areas(project)
     project.practice_area_names.split(',').map(&:strip)
+  end
+
+  def most_recent_article()
+    match = data.features.articles.detect { |a| a.is_most_recent }
+    puts "Couldn't find most recent article" if match.nil?
+
+    match
   end
 end
