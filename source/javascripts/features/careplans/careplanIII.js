360af2b55d4ce31c9ab6a24732fb546e47243697
YvonneChow <naruto3135@gmail.com>
2016-03-10 17:30:20 -0500

    Intro page: remove email button

--- source/javascripts/features/careplans/careplanIII.js
+++ source/javascripts/features/careplans/careplanIII.js
@@ -109,4 +109,29 @@ $(document).ready(function() {
 	   }
 	});
 
+// function getDeviceScale() {
+//     var deviceWidth, landscape = Math.abs(window.orientation) == 90;
+//     if (landscape) {
+//       // iPhone OS < 3.2 reports a screen height of 396px
+//       deviceWidth = Math.max(480, screen.height);
+//     } else {
+//       deviceWidth = screen.width;
+//       console.log(screen.width)
+//     }
+//     return window.innerWidth / deviceWidth;
+// }
+
+// // mobile only - keep the position:fixed header at constant size when page is zoomed
+// if (navigator.userAgent.match(/Mobi/)) {
+//     $(window).on('load scroll', function() {
+//         var ds = getDeviceScale();
+//         $('.device-fixed-height').css('transform','scale(1,' + ds + ')')
+//             .css('transform-origin', '0 0');
+//         $('.device-fixed-width').css('transform', 'scale(' + ds + ',1)')
+//             .css('transform-origin', '0 0');
+//         })
+// }
+
+
 });
+
